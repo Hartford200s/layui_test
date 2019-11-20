b46344a90c50e7c41d5ed5c325f1d003625f4119
tai <tai@test.com>
2019-11-20 18:23:41 +0800

    define,config 模块化sample1

---
+++ define_module_sample1/res/js/modules/module1.js
@@ -0,0 +1,10 @@
+layui.define([
+    'layer',
+    'form'
+], function(exports) {
+    var layer = layui.layer;
+    var form = layui.form;
+    layer.msg("hello~ this is module1.js");
+    
+    exports('module1', {});//注意，这里是模块输出的核心，模块名必须和use时的模块名一致
+});
