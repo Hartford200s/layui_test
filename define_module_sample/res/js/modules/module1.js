c424a3a4ea7afde8752e175b2b36a04a62502361
tai <tai@test.com>
2019-11-21 10:36:11 +0800

    rename files

--- define_module_sample/res/js/modules/module1.js
+++ define_module_sample/res/js/modules/module1.js
@@ -5,6 +5,7 @@ layui.define([
     var layer = layui.layer;
     var form = layui.form;
     layer.msg("hello~ this is module1.js");
-    
     exports('module1', {});//注意，这里是模块输出的核心，模块名必须和use时的模块名一致
 });
+
+
